33399988e6644d9f32505fbef218aa766077a660
Codey Pinero <codey.pinero@gmail.com>
2019-03-25 17:12:15 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,9 +3,5 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  array = [1, 2, 3, 4, 5, 6]
-
-  array.each do |number|
-    puts number
-  end
+  (1..6).to_a.sample
 end
